a144298663706bb61597be14210669a1512fd077
ade bakari <redariez@gmail.com>
2020-04-28 10:05:04 -0700

    Form Validation complete

--- app.js
+++ app.js
@@ -19,11 +19,14 @@ function showSuccess(input) {
 }
 
 //Check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+   if(re.test(input.value.trim())) {
+    showSuccess();
+   } else {
+     showError(input, 'Email is not valid');  
+   }
 }
-
 //Check required fields
 function checkRequired(inputArr) {
     inputArr.forEach(function(input) {
@@ -33,6 +36,31 @@ function checkRequired(inputArr) {
             showSuccess(input);
         }
     });
+}
+
+//Check input length
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(
+            input, 
+            `${getFieldName(input)} must be at least ${min} characters`
+        );
+    } else if(input.value.length > max) {
+        showError(
+            input, 
+            `${getFieldName(input)} must be at least ${max} characters`
+        );
+    } else {
+        showSuccess(input);
+    }
+    
+}
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+    if(input1.value !== input2.value ) {
+        showError(input2, 'Passwords do not match')
+    }
 }
 
 //Get fieldname
@@ -45,4 +73,8 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
 });
